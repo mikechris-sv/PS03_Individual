23a39a5f65303be09e921e25ecd173476db489ab
mikechris-sv <ferminmikechristian17@gmail.com>
2024-09-21 13:48:04 +0800

    code added

---
+++ PS_03_LeapYear/PS_03_LeapYear/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace PS_03_LeapYear
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a year: ");
+            int year = int.Parse(Console.ReadLine());
+
+            bool isLeapYear = LeapYear(year);
+            Console.WriteLine($"Does {year} year a leap year? {isLeapYear} ");
+
+        }
+        public static bool LeapYear(int year)
+        {
+            if (year % 4 == 0)
+            {
+                if (year % 100 == 0)
+                {
+                    return year % 400 == 0;
+                }
+                return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ PS_03_LeapYear/PS_03_TriangleArea/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace PS_03_TriangleArea
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
